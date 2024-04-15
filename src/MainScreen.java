32d4c648c18c0e93e2186cc32d22acfd7623f9e8
Darserg <Darserg1212>
2024-04-15 08:29:26 +0400

    Base

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,12 +1,15 @@
+import com.sun.tools.javac.Main;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Array;
 import java.util.*;
 
-public class MainScreen {
+public class MainScreen extends  JFrame implements ActionListener {
 
     static JFrame frame;
     static JTextField exercize;
@@ -14,9 +17,69 @@ public class MainScreen {
     static ArrayList<String> operations = new ArrayList<>();
     static String title = "";
     static int answer = 0;
+    static MainScreen listener = new MainScreen();
 
     public static void main(String[] args) {
-//        Main listen = new Main();
+        update();
+    }
+
+    public static ArrayList<JButton> generateButtons() {
+        ArrayList<JButton> buttons = new ArrayList<>();
+        ArrayList<Integer> numbers = generateNum();
+        for (int i = 0; i < 4; i++) {
+            JButton jTmpButton = new JButton(Integer.toString(numbers.get(i)));
+            jTmpButton.addActionListener(listener);
+            buttons.add(jTmpButton);
+        }
+        return buttons;
+    }
+
+    public static int randomNumber() {
+        int minValue = 1;
+        int maxValue = 10;
+        return minValue + (int) (Math.random() * (maxValue - minValue + 1));
+    }
+
+    public static @NotNull ArrayList<Integer> generateNum() {
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        int countAns = 0;
+        for (int i = 0; i < 4; i++) {
+            int temp = randomNumber();
+            if (temp == answer) {
+                countAns += 1;
+                if (countAns > 1) {
+                    i -= 1;
+                } else {
+                    num.add(temp);
+                }
+            } else {
+                if ((i == 3) && (countAns == 0)) {
+                    num.add(answer);
+                }
+                else {
+                    num.add(temp);
+                }
+            }
+        }
+        return num;
+    }
+
+    // Обработка события
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (answer == Integer.parseInt(e.getActionCommand())) {
+            System.out.println("You got a correct answer");
+            windowPreparation(frame);
+        }
+        else {
+            System.out.println("You got incorrect answer, try again");
+            System.out.println(e.getID());
+        }
+    }
+
+
+    // Отрисовка пользовательского интерфейса
+    public static void update() {
         operations.add("+");
         operations.add("-");
 
@@ -27,13 +90,13 @@ public class MainScreen {
 
         // Task to complete
         if (firstNum > secondNum) {
-           title  = String.format("%d %s %d", firstNum, operation, secondNum);
-           if (Objects.equals(operation, "+")) {
-               answer = firstNum + secondNum;
-           }
-           else {
-               answer = firstNum - secondNum;
-           }
+            title  = String.format("%d %s %d", firstNum, operation, secondNum);
+            if (Objects.equals(operation, "+")) {
+                answer = firstNum + secondNum;
+            }
+            else {
+                answer = firstNum - secondNum;
+            }
         }
         else if (firstNum < secondNum) {
             title = String.format("%d %s %d", secondNum, operation, firstNum);
@@ -76,53 +139,14 @@ public class MainScreen {
         frame.add(display);
         frame.setSize(1000, 500);
         frame.setVisible(true);
-
-
     }
 
-    public static ArrayList<JButton> generateButtons() {
-        ArrayList<JButton> buttons = new ArrayList<>();
-        ArrayList<Integer> numbers = generateNum();
-        for (int i = 0; i < 4; i++) {
-            JButton jTmpButton = new JButton(Integer.toString(numbers.get(i)));
-//            jTmpButton.addActionListener(listen);
-            buttons.add(jTmpButton);
-        }
-        return buttons;
-    }
 
-    public static int randomNumber() {
-        int minValue = 1;
-        int maxValue = 10;
-        return minValue + (int) (Math.random() * (maxValue - minValue + 1));
+    // Очистка экрана при правильном ответе
+    public static void windowPreparation(JFrame window) {
+        window.removeAll();
+        window.validate();
+        window.repaint();
+        update();
     }
-
-    public static @NotNull ArrayList<Integer> generateNum() {
-        ArrayList<Integer> num = new ArrayList<Integer>();
-        int countAns = 0;
-        for (int i = 0; i < 4; i++) {
-            int temp = randomNumber();
-            if (temp == answer) {
-                countAns += 1;
-                if (countAns > 1) {
-                    i -= 1;
-                } else {
-                    num.add(temp);
-                }
-            } else {
-                if ((i == 3) && (countAns == 0)) {
-                    num.add(answer);
-                }
-                else {
-                    num.add(temp);
-                }
-            }
-        }
-        return num;
-    }
-
-//    @Override
-//    public void actionPerfomed(ActionEvent e) {
-//        System.out.println(e.getActionCommand);
-//    }
 }
